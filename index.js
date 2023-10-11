e7bf070814fc70a1bb0b2452ff12e2a3a138788e
paulmkrtichian <paulmkr22@gmail.com>
2023-10-11 09:21:22 -0400

    10/11

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const arr2 = {
+/* const arr2 = {
     fname:"John",
     lname:"Doe",
     age: 25,
@@ -11,3 +11,27 @@ console.log(arr4);
 
 const arr3 = [true, false, "yay!", [1,4,8], null, 5];
 console.log(arr3);
+*/
+
+const students = [
+    {
+        fName: "Alex",
+        lName: "Mkrtichian",
+        height: "6'0",
+        graduated: true,
+    },
+    {
+        fName: "Paul",
+        lName: "Mkrtichian",
+        height: "15",
+        graduated: true,
+    },
+    {
+        fName: "Josh",
+        lName: "Mkrtichian",
+        height: "5'2",
+        graduated: true,
+    }
+]
+students.forEach((students) => console.log(students.fName + " " + students.lName));
+
