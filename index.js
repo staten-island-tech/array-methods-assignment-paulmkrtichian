6a894e08580af2c0468efdf6818f19808a5466da
paulmkrtichian <paulmkr22@gmail.com>
2023-10-11 22:23:06 -0400

    10/11 2

--- index.js
+++ index.js
@@ -13,25 +13,45 @@ const arr3 = [true, false, "yay!", [1,4,8], null, 5];
 console.log(arr3);
 */
 
+
 const students = [
     {
         fName: "Alex",
         lName: "Mkrtichian",
-        height: "6'0",
-        graduated: true,
+        heights: ['7','1'],
+        graduations: ['2007','2011'],
+        grade: 10,
     },
     {
         fName: "Paul",
-        lName: "Mkrtichian",
-        height: "15",
-        graduated: true,
+        lName: "Jones",
+        heights: ['6','8'],
+        graduations: ['2008',2010],
+        grade: 9,
     },
     {
         fName: "Josh",
         lName: "Mkrtichian",
-        height: "5'2",
-        graduated: true,
+        heights: ['6','5'],
+        graduations: ['2008','2009'],
+        grade: 10,
     }
 ]
 students.forEach((students) => console.log(students.fName + " " + students.lName));
 
+students.forEach((students) => {
+    console.log(students.fName + " " + students.lName + 'graduations:');
+    students.graduations.forEach((graduated) => {
+        console.log(graduated);
+    });
+});
+
+students.forEach((students) => {
+    console.log(students.fName + " " + students.lName + 'heights:');
+    students.heights.forEach((height) => {
+        console.log(height);
+    });
+});
+
+const tenthgrade = students.filter((student) => student.grade === 10);
+console.log(tenthgrade)
