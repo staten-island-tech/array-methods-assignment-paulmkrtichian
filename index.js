d62b2d39912dc8a174fab8a0de7043bceba71985
paulmkrtichian <paulmkr22@gmail.com>
2023-10-13 22:41:43 -0400

    10/11

--- index.js
+++ index.js
@@ -13,45 +13,51 @@ const arr3 = [true, false, "yay!", [1,4,8], null, 5];
 console.log(arr3);
 */
 
-
-const students = [
+const MMAfighters = [
     {
-        fName: "Alex",
-        lName: "Mkrtichian",
-        heights: ['7','1'],
-        graduations: ['2007','2011'],
-        grade: 10,
-    },
-    {
-        fName: "Paul",
+        fName: "John",
         lName: "Jones",
-        heights: ['6','8'],
-        graduations: ['2008',2010],
-        grade: 9,
+        record: ['27 wins','1 loss'],
+        heightweight: ['193.04 cm','248 lbs'],
+        fromRussia: "No",
     },
     {
-        fName: "Josh",
-        lName: "Mkrtichian",
-        heights: ['6','5'],
-        graduations: ['2008','2009'],
-        grade: 10,
-    }
+        fName: "Khabib",
+        lName: "Nurmagomedov",
+        record: ['29 wins','0 losses'],
+        heightweight: ['177.8 cm','155 lbs'],
+        fromRussia: "Yes",
+    },
+    {
+        fName: "Islam",
+        lName: "Makhachev",
+        record: ['24 wins','1 loss'],
+        heightweight: ['177.8 cm','155 lbs'],
+        fromRussia: "Yes",
+    },
+    {
+        fName: "Charles",
+        lName: "Oliverira",
+        record: ['34 wins','9 loss'],
+        heightweight: ['177.8 cm','154 lbs'],
+        fromRussia: "No",
+    },
 ]
-students.forEach((students) => console.log(students.fName + " " + students.lName));
+MMAfighters.forEach((MMAfighters) => console.log(MMAfighters.fName + " " + MMAfighters.lName));
 
-students.forEach((students) => {
-    console.log(students.fName + " " + students.lName + ' graduations:');
-    students.graduations.forEach((graduated) => {
-        console.log(graduated);
+MMAfighters.forEach((MMAfighters) => {
+    console.log(MMAfighters.fName + " " + MMAfighters.lName + ' record:');
+    MMAfighters.record.forEach((recordwinloss) => {
+        console.log(recordwinloss);
     });
 });
 
-students.forEach((students) => {
-    console.log(students.fName + " " + students.lName + ' heights:');
-    students.heights.forEach((height) => {
-        console.log(height);
+MMAfighters.forEach((MMAfighters) => {
+    console.log(MMAfighters.fName + " " + MMAfighters.lName + ' heightweight:');
+    MMAfighters.heightweight.forEach((heightincmsweightinlbs) => {
+        console.log(heightincmsweightinlbs);
     });
 });
 
-const tenthgrade = students.filter((student) => student.grade === 10);
-console.log(tenthgrade)
+const Russian = MMAfighters.filter((MMAfighters) => MMAfighters.fromRussia === 'Yes');
+console.log(Russian);
